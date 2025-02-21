1efd8cbf116c1d6681d420d1568ada613d7172b9
RubenSuarez <rubensuarez29@gmail.com>
2025-02-20 21:16:16 -0500

    Factura - Calculo de la tarifa a pagar

--- src/factura.php
+++ src/factura.php
@@ -12,6 +12,31 @@
     $cc="";
     $phonecliente="";
     $email="";
+    $modelo="";
+    $marca="";
+    $placa='';
+    $fechaentrada="";
+    $fechasalida="";
+    $horastranscurridas=0;
+    $precio=0;
+    $valorpaga=0;
+
+
+
+    function calcularHoras($fechaInicio, $fechaFin) {
+        // Crear objetos DateTime a partir de las cadenas de fecha
+        $inicio = new DateTime($fechaInicio);
+        $fin = new DateTime($fechaFin);
+    
+        // Calcular la diferencia entre las fechas
+        $diferencia = $inicio->diff($fin);
+    
+        // Obtener la diferencia en horas
+        $horas = ($diferencia->days * 24) + $diferencia->h;
+    
+        return $horas;
+    }
+
 
     if(!empty($_GET)){
 
@@ -31,11 +56,12 @@
         $sqlhistoria="select historial_puesto.idpuesto, historial_puesto.created_at, ";
         $sqlhistoria=$sqlhistoria ."historial_puesto.updated_at, tbusuarios.NOMBRES as name, tbusuarios.APELLIDOS, ";
         $sqlhistoria=$sqlhistoria ."tbusuarios.IDENTIFICACION as cedula, tbusuarios.CELULAR, tbusuarios.EMAIL, ";
-        $sqlhistoria=$sqlhistoria ."vehiculo.marca,vehiculo.modelo, vehiculo.placa, tipovehiculo.vehiculo ";
+        $sqlhistoria=$sqlhistoria ."vehiculo.marca,vehiculo.modelo, vehiculo.placa, tipovehiculo.vehiculo, tarifa.precio ";
         $sqlhistoria=$sqlhistoria ."from historial_puesto ";
         $sqlhistoria=$sqlhistoria ."INNER JOIN tbusuarios ON historial_puesto.idusuario=tbusuarios.ID_USUARIO ";
         $sqlhistoria=$sqlhistoria ."INNER JOIN vehiculo ON historial_puesto.idvehiculo=vehiculo.id_vehiculo ";
         $sqlhistoria=$sqlhistoria ."INNER JOIN tipovehiculo ON vehiculo.id_tipovehiculo=tipovehiculo.id_tipo ";
+        $sqlhistoria=$sqlhistoria ." INNER JOIN tarifa ON tipovehiculo.id_tipo=tarifa.tipoVehiculo ";
         $sqlhistoria=$sqlhistoria ." WHERE historial_puesto.idpuesto=$idpuesto";
 
         //echo $sqlhistoria;
@@ -48,6 +74,15 @@
             $cc=$fila1['cedula'];
             $phonecliente=$fila1['CELULAR'];
             $email=$fila1['EMAIL'];
+            $marca=$fila1['marca'];
+            $modelo=$fila1['modelo'];
+            $placa=$fila1['placa'];
+            $fechaentrada=$fila1['created_at'];
+            $fechasalida=date("Y-m-d H:i:s");
+            $horasTranscurridas = calcularHoras($fechaentrada, $fechasalida);
+            $precio=$fila1['precio'];
+            $valorpaga=$horasTranscurridas*$precio;
+
         }
         
 
@@ -73,10 +108,17 @@
             <p><label for="nit">NIT: <?= $nit ?> </label></p>
             <p><label for="direccion">Direccion: <?= $direccion ?> </label></p>
             <p><label for="telefono">Telefono: <?= $phone ?></label></p>
-            <p><label for="nombre">Nombre del Cliente<?= $nombre ?></label></p>
+            <p><label for="nombre">Nombre del Cliente: <?= $nombre ?></label></p>
             <p><label for="cc">Identificación: <?= $cc ?> </label></p>
             <p><label for="phone">Telefono del cliente: <?= $phonecliente ?> </label></p>
             <p><label for="email">Email: <?= $email ?></label></p>
+            <p><label for="nombre">Placa :<?= $placa ?></label></p>
+            <p><label for="cc">Marca: <?= $marca ?> </label></p>
+            <p><label for="modelo">Modelo: <?= $modelo ?> </label></p>
+            <p><label for="fechaentrada">Fecha de Entrada: <?= $fechaentrada ?></label></p>
+            <p><label for="fechasalida">Fecha de Salida <?= $fechasalida ?> </label></p>
+            <p><label for="hora">Hora: <?= $horasTranscurridas ?></label></p>
+            <p><label for="valor">Valor a pagar: <?= $valorpaga ?></label></p>
 
             <p>
             <input type="submit" name="Guardar" value="Guardar">
